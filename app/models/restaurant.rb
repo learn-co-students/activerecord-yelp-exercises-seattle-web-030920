fefd6cd15c9a816f96528f3812a5f45d002c9de3
fosterv2 <fosterv2@outlook.com>
2020-09-11 13:02:19 -0700

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -12,13 +12,27 @@ class Restaurant < ActiveRecord::Base
         self.first(10).last
     end
 
-    # def self.with_long_names
-    #     self.
-    # end
+    def self.with_long_names
+        self.where('LENGTH(name) > 12')
+    end
 
     def self.max_dishes
     end
 
     def self.focused
+        self.joins(:dishes).having('COUNT(dishes.id) < 5').group(:restaurant_id)
+    end
+
+    def large_menu
+        self.joins(:dishes).having('COUNT(dishes.id) > 20').group(:restaurant_id)
+    end
+
+    def vegetarian
+    end
+
+    def name_like(name)
+    end
+
+    def name_not_like(name)
     end
 end
